390ddf57cfec98714979fa26f45fdb946c9a93f2
baekjung <zvgandam@gmail.com>
2019-10-22 12:12:33 +0900

    apollo Client Update

--- src/apollo/apolloClient.js
+++ src/apollo/apolloClient.js
@@ -1,15 +1,29 @@
-import ApolloClient from 'apollo-boost';
-import { defaults, resolvers } from 'apollo/localState';
+import ApolloClient from 'apollo-client';
+import { createPersistedQueryLink } from 'apollo-link-persisted-queries';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { resolvers } from 'apollo/localState';
+
+const cache = new InMemoryCache();
+cache.writeData({
+  data: {
+    logged: Boolean(localStorage.getItem('token')) || false,
+  },
+});
+
+const link = ApolloLink.from([
+  createPersistedQueryLink({ useGETForHashedQueries: true }),
+  createHttpLink({
+    uri: process.env.REACT_APP_BACKEND_URL,
+    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+  }),
+]);
 
 const Client = new ApolloClient({
-  uri: process.env.REACT_APP_MONGO_URL,
-  clientState: {
-    defaults,
-    resolvers,
-  },
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('token')}`,
-  },
+  link,
+  cache,
+  resolvers,
 });
 
 export default Client;
