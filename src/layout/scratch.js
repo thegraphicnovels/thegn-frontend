7e03f3df4ed3a7ad875d131697cdf6f60924df7e
baekjung <zvgandam@gmail.com>
2019-10-22 17:20:49 +0900

    .

--- src/layout/scratch.js
+++ src/layout/scratch.js
@@ -1,35 +1,41 @@
 import React, { useRef, useEffect, useState } from 'react';
 import Login from 'page/login';
-import { scratchFn, layerOpenFn } from 'common';
+import { scratchFn } from 'common';
 
-const Scratch = ()=> {
-	const scratchWrap = useRef(null);
-	const [isScratchEnd, setScratchEnd] = useState(false);
-	const [isPopOpen, setPopOpen] = useState('none');
-	
-	useEffect(()=> {
-		if(!isScratchEnd) {
-			scratchFn(scratchWrap.current, 60, ()=> {
-				// scratch end function
-				console.log('scratch end');
-				setScratchEnd(true);
-			});
-		}
-	}, [isScratchEnd]);
+const Scratch = () => {
+  const scratchWrap = useRef(null);
+  const [isScratchEnd, setScratchEnd] = useState(false);
+  const [isPopOpen, setPopOpen] = useState('none');
 
-	useEffect(()=> {
-		if(isPopOpen === 'none') {
-			setScratchEnd(false);
-		}
-	}, [isPopOpen]);
-	
-	return(
-		<div className="scratchWrap" ref={scratchWrap}>
-			{!isScratchEnd && (<canvas className="canvas" width={150} height={80} />)}
-			<button type="button" onClick={()=> setPopOpen('block')} className="ScratchCard__Result">Admin Login</button>
-			{isScratchEnd && (<Login isPopOpen={isPopOpen} setPopOpen={setPopOpen} />)}
-		</div>
-	);
-}
+  useEffect(() => {
+    if (!isScratchEnd) {
+      scratchFn(scratchWrap.current, 60, () => {
+        // scratch end function
+        console.log('scratch end');
+        setScratchEnd(true);
+      });
+    }
+  }, [isScratchEnd]);
+
+  useEffect(() => {
+    if (isPopOpen === 'none') {
+      setScratchEnd(false);
+    }
+  }, [isPopOpen]);
+
+  return (
+    <div className="scratchWrap" ref={scratchWrap}>
+      {!isScratchEnd && <canvas className="canvas" width={150} height={80} />}
+      <button
+        type="button"
+        onClick={() => setPopOpen('block')}
+        className="ScratchCard__Result"
+      >
+        Admin Login
+      </button>
+      {isScratchEnd && <Login isPopOpen={isPopOpen} setPopOpen={setPopOpen} />}
+    </div>
+  );
+};
 
 export default Scratch;
