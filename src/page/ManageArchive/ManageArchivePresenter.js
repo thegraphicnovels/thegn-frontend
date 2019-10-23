cbae673467764112a27dc8456e86e1c4dd89f952
Seo hyunh ho <hyunh84@hanmail.net>
2019-10-23 16:50:55 +0900

    archive Admin

--- src/page/ManageArchive/ManageArchivePresenter.js
+++ src/page/ManageArchive/ManageArchivePresenter.js
@@ -25,46 +25,72 @@ const ManageArchivePresenter = ({
   findDuplicates,
 }) => {
 	return (
-		<div>
+		<div className="registBox">
+			<h2>Archive</h2>
 			<table>
 				<caption>섬네일 이미지 등록</caption>
+				<colgroup>
+					<col style={{width:'25%'}} />
+					<col style={{width:'75%'}} />
+				</colgroup>
 				<tbody>
 					<tr>
-						<th>Thumbnail image</th>
+						<th>Title</th>
+						<td>
+							<input id="archiveTitle" placeholder="Title" value={title.value} onChange={title.onChange} type="text" />
+						</td>
+					</tr>
+					<tr>
+						<th>Description</th>
+						<td>
+							<textarea placeholder="Description" value={description.value} onChange={description.onChange} />
+						</td>
+					</tr>
+					<tr>
+						<th>Thumbnail</th>
 						{!thumbFileUrl.length && (
 							<td>
-								<FilePond filepondEl={thumbFilepondEl} files={thumbFiles} setFiles={setThumbFiles} allowMultiple={false} label="Drag & Drop File" />
+								<FilePond filepondEl={thumbFilepondEl} files={thumbFiles} setFiles={setThumbFiles} allowMultiple={false} label="Drag & Drop Image" />
 							</td>
 						)}
 						{thumbFileUrl.length > 0 && (
 							<td>
-								<div style={{ display: 'flex', flexdirection: 'row' }}>
-									<div>
-										<img src={thumbFileUrl[0]} alt={thumbFileUrl[0]} style={{ width: '500px', height: '500px', padding: '10px' }} />
-										<button type="button" className="btnDel" onClick={() => setThumbFileUrl('')}>삭제</button>
-									</div>
+								<div className="previewThumbImgBox">
+									<span><img src={thumbFileUrl[0]} alt={thumbFileUrl[0]} /></span>
+									<button type="button" onClick={() => setThumbFileUrl('')}>
+										<em className="blind">삭제</em>
+										<svg width="26" height="26" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><path d="M11.586 13l-2.293 2.293a1 1 0 0 0 1.414 1.414L13 14.414l2.293 2.293a1 1 0 0 0 1.414-1.414L14.414 13l2.293-2.293a1 1 0 0 0-1.414-1.414L13 11.586l-2.293-2.293a1 1 0 0 0-1.414 1.414L11.586 13z" fill="currentColor" fillRule="nonzero" /></svg>
+									</button>
 								</div>
 							</td>
 						)}
 					</tr>
+					<tr>
+						<th>Portpolio</th>
+						<td>
+							<FilePond filepondEl={filepondEl} files={files} setFiles={setFiles} allowMultiple label="Drag & Drop Image" />
+							
+							{portpolioData && (
+								<ul className="regImgViewList">
+									{fileUrl.map((file, i) => (
+										<li key={i}>
+											<div className="previewThumbImgBox">
+												<span><img src={file} alt={file} /></span>
+												<button type="button" onClick={() => handleDelFile(i)}>
+													<em className="blind">삭제</em>
+													<svg width="26" height="26" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><path d="M11.586 13l-2.293 2.293a1 1 0 0 0 1.414 1.414L13 14.414l2.293 2.293a1 1 0 0 0 1.414-1.414L14.414 13l2.293-2.293a1 1 0 0 0-1.414-1.414L13 11.586l-2.293-2.293a1 1 0 0 0-1.414 1.414L11.586 13z" fill="currentColor" fillRule="nonzero" /></svg>
+												</button>
+											</div>
+										</li>
+									))}
+								</ul>
+							)}
+
+						</td>
+					</tr>
 				</tbody>
 			</table>
-
-			<FilePond filepondEl={filepondEl} files={files} setFiles={setFiles} allowMultiple label="Drag & Drop Portpolio File" />
-			{portpolioData && (
-			<div style={{ display: 'flex', flexdirection: 'row' }}>
-				{fileUrl.map((file, i) => (
-				<div key={i}>
-					<img src={file} alt={file} style={{ width: '500px', height: '500px', padding: '10px' }} />
-					<button type="button" onClick={() => handleDelFile(i)}>
-					삭제
-					</button>
-				</div>
-				))}
-			</div>
-			)}
-			<input id="archiveTitle" placeholder="Title" value={title.value} onChange={title.onChange} type="text" />
-			<textarea placeholder="Description" value={description.value} onChange={description.onChange} />
+			
 
 			{tagData &&
 			tagData.seeTags.map(tag => {
@@ -78,20 +104,22 @@ const ManageArchivePresenter = ({
 				);
 			})}
 
-			{!portpolioData ? (
-			<button type="button" onClick={() => handleUpload('upload')}>
-				Upload Archive
-			</button>
-			) : (
-			<button type="button" onClick={() => handleUpload('edit')}>
-				Edit Archive
-			</button>
-			)}
-			{portpolioData && (
-			<button type="button" onClick={() => handleDeleteArchive(portpolioData.detailPortpolio._id)}>
-				Delete Archive
-			</button>
-			)}
+			<div className="btnPageBox">
+				{!portpolioData ? (
+				<button className="btnCustm01" type="button" onClick={() => handleUpload('upload')}>
+					<span>Upload Archive</span>
+				</button>
+				) : (
+				<button className="btnCustm01" type="button" onClick={() => handleUpload('edit')}>
+					<span>Edit Archive</span>
+				</button>
+				)}
+				{portpolioData && (
+				<button className="btnCustm01" type="button" onClick={() => handleDeleteArchive(portpolioData.detailPortpolio._id)}>
+					<span>Delete Archive</span>
+				</button>
+				)}
+			</div>
 		</div>
 	);
 };
