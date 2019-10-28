dd5201c7b0832585d1420364a6247b553e50bb4a
seo hyun ho <hyunh84@hanmail.net>
2019-10-28 13:59:42 +0900

    반응형 작업

--- src/page/main.js
+++ src/page/main.js
@@ -20,31 +20,50 @@ const Main = ({
 
 	return (
 		<div className="contents" ref={conts}>
-		<div className="pageWrap" style={{ display: 'block', width: '100%', height: 'auto' }}>
-			<h2 className="blind">Home</h2>
-			<Mainswiper action={action} />
-		</div>
-		<button type="button" className="subMenu01" onClick={() => {menuClick(conts, setAction, 1);}}>
-			<em>&lt;Archive&gt;</em>
-		</button>
-		<div className="pageWrap">
-			<h2 className="blind">Archive</h2>
-			<Achive action={action} />
-		</div>
-		<button type="button" className="subMenu02" onClick={() => {menuClick(conts, setAction, 2);}}>
-			<em>&lt;About&gt;</em>
-		</button>
-		<div className="pageWrap">
-			<h2 className="blind">About</h2>
-			<About action={action} />
-		</div>
-		<button type="button" className="subMenu03" onClick={() => {menuClick(conts, setAction, 3);}}>
-			<em>&lt;Contact&gt;</em>
-		</button>
-		<div className="pageWrap">
-			<h2 className="blind">Contact</h2>
-			<Contact action={action} />
-		</div>
+			<nav className="naviListWrap">
+				<ul>
+					<li>
+						<button type="button" onClick={() => {menuClick(conts, setAction, 1);}}>
+							<em>&lt;Archive&gt;</em>
+						</button>
+					</li>
+					<li>
+						<button type="button" onClick={() => {menuClick(conts, setAction, 2);}}>
+							<em>&lt;About&gt;</em>
+						</button>
+					</li>
+					<li>
+						<button type="button" onClick={() => {menuClick(conts, setAction, 3);}}>
+							<em>&lt;Contact&gt;</em>
+						</button>
+					</li>
+				</ul>
+			</nav>
+			<div className="pageWrap" style={{ display: 'block', width: '100%', height: 'auto' }}>
+				<h2 className="blind">Home</h2>
+				<Mainswiper action={action} />
+			</div>
+			<button type="button" className="subMenu01" onClick={() => {menuClick(conts, setAction, 1);}}>
+				<em>&lt;Archive&gt;</em>
+			</button>
+			<div className="pageWrap">
+				<h2 className="blind">Archive</h2>
+				<Achive action={action} />
+			</div>
+			<button type="button" className="subMenu02" onClick={() => {menuClick(conts, setAction, 2);}}>
+				<em>&lt;About&gt;</em>
+			</button>
+			<div className="pageWrap">
+				<h2 className="blind">About</h2>
+				<About action={action} />
+			</div>
+			<button type="button" className="subMenu03" onClick={() => {menuClick(conts, setAction, 3);}}>
+				<em>&lt;Contact&gt;</em>
+			</button>
+			<div className="pageWrap">
+				<h2 className="blind">Contact</h2>
+				<Contact action={action} />
+			</div>
 		</div>
 	);
 };
