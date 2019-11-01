4b04ab60996deec53d7394665037d83100274b97
seo hyun ho <hyunh84@hanmail.net>
2019-11-01 11:29:34 +0900

    related

---
+++ src/components/related_list.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const RelatedList = ({related, portpolioId})=> {
+
+	console.log(related);
+
+	return(
+		<div className="archiveMoreBox">
+			<h3>MORE PROJECT</h3>
+
+			<button type="button" className="btnPrev"><em className="blind">이전</em></button>
+			<div className="moreListWrap">
+				<ul className="swiper-wrapper">
+					{related.map(relatedData => {
+						if (portpolioId !== relatedData._id) {
+							return (
+								<li key={relatedData._id} className="swiper-slide">
+									<Link to={`/archiveDetail/${relatedData._id}`}>
+										<img src={relatedData.thumbImg} alt={relatedData.title} />
+
+										<div className="itemFrame">
+											<div className="inner">
+												<span className="tits">{relatedData.title}</span>
+												<span className="tags">
+													{relatedData.tags.length > 0 &&
+													relatedData.tags.map((item, i) => {
+														if (i === 0) {
+														return item.value;
+														}
+														return `, ${item.value}`;
+													})}
+												</span>
+											</div>
+										</div>
+									</Link>
+								</li>
+							);
+						}
+					})}
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+RelatedList.propTypes = {
+	related : PropTypes.array.isRequired,
+	portpolioId : PropTypes.string.isRequired,
+}
+
+export default RelatedList;
--- src/page/archive_detail.js
+++ src/page/archive_detail.js
@@ -8,12 +8,13 @@ import {
 } from 'apollo/archiveQuery';
 import { swiperFn, formatDate } from 'common';
 import NaviList from 'components/naviList';
+import RelatedList from 'components/related_list';
 import { Store } from 'store';
 
 const ArchiveDetail = ({
   history,
   match: {
-    params: { portpolioId },
+	params: { portpolioId },
   },
 }) => {
   //   console.log('archiveDetail ID', portpolioId);
@@ -27,145 +28,124 @@ const ArchiveDetail = ({
 
   // 연관 tag Archive
   const [relatedArchiveFn] = useLazyQuery(archiveListQuery, {
-    fetchPolicy: 'network-only',
-    onCompleted: async ({ seePortpoliosList }) => {
-      setRelated(seePortpoliosList);
-    },
+	fetchPolicy: 'network-only',
+	onCompleted: async ({ seePortpoliosList }) => {
+	  setRelated(seePortpoliosList);
+	  console.log(seePortpoliosList);
+	},
   });
 
   const { data, loading } = useQuery(archiveDetailQuery, {
-    variables: { id: portpolioId },
-    fetchPolicy: 'network-only',
-    onCompleted: ({ detailPortpolio }) => {
-      archiveViewsMutation({ variables: { id: portpolioId } });
-      const { tags } = detailPortpolio;
-      if (tags) {
-        const tagIds = [];
-        for (let i = 0; i < tags.length; i++) {
-          tagIds.push(tags[i]._id);
-        }
-        relatedArchiveFn({ variables: { tags: tagIds } });
-      }
-    },
+	variables: { id: portpolioId },
+	fetchPolicy: 'network-only',
+	onCompleted: ({ detailPortpolio }) => {
+	  archiveViewsMutation({ variables: { id: portpolioId } });
+	  const { tags } = detailPortpolio;
+	  if (tags) {
+		const tagIds = [];
+		for (let i = 0; i < tags.length; i++) {
+		  tagIds.push(tags[i]._id);
+		}
+		relatedArchiveFn({ variables: { tags: tagIds } });
+	  }
+	},
   });
 
   // 뒤로가기 클릭시
   useEffect(() => {
-    window.onpopstate = e => {
-      setAction(1);
-      history.push({ pathname: '/' });
-    };
+	window.onpopstate = e => {
+	  setAction(1);
+	  history.push({ pathname: '/' });
+	};
   }, [history, setAction]);
 
   useEffect(() => {
-    if (!loading) {
-      swiperFn(topSwiperEl.current);
-    }
+	if (!loading) {
+	  swiperFn(topSwiperEl.current);
+	}
   }, [loading]);
 
   if (!loading) {
-    return (
-      <div className="contents archiveDetail">
-        <span id="jumpConts" className="blind">
-          상세 본문영역
-        </span>
+	return (
+	  <div className="contents archiveDetail">
+		<span id="jumpConts" className="blind">
+		  상세 본문영역
+		</span>
 
-        <NaviList />
+		<NaviList />
 
-        <Link to="/" onClick={() => setAction(1)} className="subMenu01">
-          <em>&lt;Archive&gt;</em>
-        </Link>
+		<Link to="/" onClick={() => setAction(1)} className="subMenu01">
+		  <em>&lt;Archive&gt;</em>
+		</Link>
 
-        <div style={{ display: 'block', width: '100%', height: 'auto' }}>
-          <h2 className="blind">Archive</h2>
+		<div style={{ display: 'block', width: '100%', height: 'auto' }}>
+		  <h2 className="blind">Archive</h2>
 
-          <div className="archiveDescriptWrap">
-            <div className="descriptThum" ref={topSwiperEl}>
-              <button type="button" className="btnPrev">
-                <em className="blind">이전</em>
-              </button>
-              <ul className="swiper-wrapper">
-                {data &&
-                  data.detailPortpolio.files.map(imgUrl => (
-                    <li key={imgUrl._id} className="swiper-slide">
-                      <span>
-                        <img src={imgUrl.url} alt="" />
-                      </span>
-                    </li>
-                  ))}
-              </ul>
-              <button type="button" className="btnNext">
-                <em className="blind">다음</em>
-              </button>
-            </div>
+		  <div className="archiveDescriptWrap">
+			<div className="descriptThum" ref={topSwiperEl}>
+			  <button type="button" className="btnPrev">
+				<em className="blind">이전</em>
+			  </button>
+			  <ul className="swiper-wrapper">
+				{data &&
+				  data.detailPortpolio.files.map(imgUrl => (
+					<li key={imgUrl._id} className="swiper-slide">
+					  <span>
+						<img src={imgUrl.url} alt="" />
+					  </span>
+					</li>
+				  ))}
+			  </ul>
+			  <button type="button" className="btnNext">
+				<em className="blind">다음</em>
+			  </button>
+			</div>
 
-            <div className="descriptBox">
-              {logged && (
-                <div className="btnDescriptBox">
-                  <Link
-                    to={`/manage/edit/archive/${data.detailPortpolio._id}`}
-                    className="btnCustm02"
-                  >
-                    <span>수정</span>
-                  </Link>
-                </div>
-              )}
-              <strong className="tit">{data.detailPortpolio.title}</strong>
+			<div className="descriptBox">
+			  {logged && (
+				<div className="btnDescriptBox">
+				  <Link
+					to={`/manage/edit/archive/${data.detailPortpolio._id}`}
+					className="btnCustm02"
+				  >
+					<span>수정</span>
+				  </Link>
+				</div>
+			  )}
+			  <strong className="tit">{data.detailPortpolio.title}</strong>
 
-              <p className="date">
-                {formatDate(data.detailPortpolio.updateAt)}
-              </p>
-              <p className="tag">
-                {data.detailPortpolio.tags &&
-                  data.detailPortpolio.tags.map((tag, i) => {
-                    if (i === 0) {
-                      return `${tag.value}`;
-                    }
-                    return `, ${tag.value}`;
-                  })}
-              </p>
+			  <p className="date">
+				{formatDate(data.detailPortpolio.updateAt)}
+			  </p>
+			  <p className="tag">
+				{data.detailPortpolio.tags &&
+				  data.detailPortpolio.tags.map((tag, i) => {
+					if (i === 0) {
+					  return `${tag.value}`;
+					}
+					return `, ${tag.value}`;
+				  })}
+			  </p>
 
-              <div className="txt">{data.detailPortpolio.description}</div>
-            </div>
-          </div>
-          {related.length > 0 &&
-            related.map(relatedData => {
-              if (portpolioId !== relatedData._id) {
-                return (
-                  <li key={relatedData._id} className="grid-item">
-                    <Link to={`/archiveDetail/${relatedData._id}`}>
-                      <img src={relatedData.thumbImg} alt={relatedData.title} />
+			  <div className="txt">{data.detailPortpolio.description}</div>
+			</div>
+		  </div>
 
-                      {/* <div className="itemFrame">
-                        <div className="inner">
-                          <span className="tits">{relatedData.title}</span>
-                          <span className="tags">
-                            {relatedData.tags.length > 0 &&
-                              relatedData.tags.map((item, i) => {
-                                if (i === 0) {
-                                  return item.value;
-                                }
-                                return `, ${item.value}`;
-                              })}
-                          </span>
-                        </div>
-                      </div> */}
-                    </Link>
-                  </li>
-                );
-              }
-              return '';
-            })}
-        </div>
+			{related.length > 0 && (
+				if(related.length === 1 && portpolioId !== relatedData[0]._id) {
+					<RelatedList related={related} portpolioId={portpolioId} />
+				}
+			)}
+		</div>
 
-        <Link to="/" onClick={() => setAction(2)} className="subMenu02">
-          <em>&lt;About&gt;</em>
-        </Link>
-        <Link to="/" onClick={() => setAction(3)} className="subMenu03">
-          <em>&lt;Contact&gt;</em>
-        </Link>
-      </div>
-    );
+		<Link to="/" onClick={() => setAction(2)} className="subMenu02">
+		  <em>&lt;About&gt;</em>
+		</Link>
+		<Link to="/" onClick={() => setAction(3)} className="subMenu03">
+		  <em>&lt;Contact&gt;</em>
+		</Link>
+	  </div>
+	);
   }
   return '';
 };
