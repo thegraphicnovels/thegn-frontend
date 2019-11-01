002bb19c2ecfa3ecbc062dca15723f6aab1bc43e
seo hyun ho <hyunh84@hanmail.net>
2019-11-01 14:10:20 +0900

    modify tagmenu click event fucntioni and add ALL menu item

--- src/components/tag_menu.js
+++ src/components/tag_menu.js
@@ -5,51 +5,63 @@ import { tagQuery } from 'apollo/tagQuery';
 import { tagMenuFn } from 'common';
 
 const TagMenu = ({ refetch, setTag }) => {
-  const tagMenuEl = useRef(null);
-  const { data: tagData, loading: tagLoading } = useQuery(tagQuery, {
-    fetchPolicy: 'network-only',
-  });
-  const chTagListFn = tag => {
-    refetch({ tags: [tag._id], page: 1 });
-    setTag(tag.value);
-  };
+	const tagMenuEl = useRef(null);
+	const { data: tagData, loading: tagLoading } = useQuery(tagQuery, {
+		fetchPolicy: 'network-only',
+	});
+	const chTagListFn = tag => {
 
-  useEffect(() => {
-    let menuFn = tagMenuEl.current;
-    if (!tagLoading && tagData) {
-      //   console.log('tagData', tagData);
-      menuFn = tagMenuFn(tagMenuEl);
-    }
+		if(tagMenuEl.current.classList.contains('active')) {
+			tagMenuEl.current.classList.remove('active');
+		}
+		if(tag === '') {
+			refetch({ tags: null, page: 1 });
+			setTag(tag);
+		}else{
+			refetch({ tags: [tag._id], page: 1 });
+			setTag(tag.value);
+		}
+	};
 
-    return () => {
-      if (menuFn) {
-        menuFn.destroy();
-      }
-    };
-  });
+	useEffect(() => {
+		let menuFn = tagMenuEl.current;
+		if (!tagLoading && tagData) {
+			//   console.log('tagData', tagData);
+			menuFn = tagMenuFn(tagMenuEl);
+		}
+
+		return () => {
+			if (menuFn) {
+				menuFn.destroy();
+			}
+		};
+	});
 
   if (!tagLoading && tagData) {
-    return (
-      <div className="tagMenu" ref={tagMenuEl}>
-        <button type="button">
-          <em className="blind">태그메뉴</em>
-        </button>
-        <div className="swiperScrollBox">
-          <div className="swiper-wrapper">
-            <ul className="swiper-slide">
-              {tagData.seeTags.map(tag => (
-                <li key={tag._id}>
-                  <button type="button" onClick={() => chTagListFn(tag)}>
-                    {tag.value}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="swiper-scrollbar" />
-        </div>
-      </div>
-    );
+	return (
+	  <div className="tagMenu" ref={tagMenuEl}>
+		<button type="button">
+		  <em className="blind">태그메뉴</em>
+		</button>
+		<div className="swiperScrollBox">
+			<div className="swiper-wrapper">
+				<ul className="swiper-slide">
+					<li>
+						<button type="button" onClick={() => chTagListFn('')}>ALL</button>
+					</li>
+					{tagData.seeTags.map((tag, i) => (
+						<li key={tag._id}>
+							<button type="button" onClick={() => chTagListFn(tag)}>
+								{tag.value}
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
+			<div className="swiper-scrollbar" />
+		</div>
+	  </div>
+	);
   }
 
   return '';
