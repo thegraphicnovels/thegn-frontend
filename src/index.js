4bd731d2cf232e1a0a651405dd6d8e4a99b5447f
baekjung <zvgandam@gmail.com>
2019-11-25 18:16:22 +0900

    Support IE11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import 'core-js/features/array/find';
+import 'core-js/features/array/includes';
+import 'core-js/features/number/is-nan';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'layout/App';
