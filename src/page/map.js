8a3d66bdcf6b19f19e31836c358dfdb1a5819c11
Seo hyunh ho <hyunh84@hanmail.net>
2020-04-25 08:43:37 +0900

    맵수정

--- src/page/map.js
+++ src/page/map.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from './mapMarker';
 
 const MapComponent = () => {
-  const center = { lat: 37.546066, lng: 127.0867 };
-  const zoom = 17;
-  return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyCjWt4pUBDu5cTtSxAq_L93PPG_prg7VEI' }}
-        defaultCenter={center}
-        defaultZoom={zoom}
-      >
-        <Marker
-          lat={37.546066}
-          lng={127.0867}
-          name="The Graphic Novels"
-          color="blue"
-        />
-      </GoogleMapReact>
-    </div>
-  );
+	const mapWrap = useRef(null);
+	const center = { lat: 37.546066, lng: 127.0867 };
+	const zoom = 17;
+	return (
+		<div className="mapWrap" ref={mapWrap}>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: 'AIzaSyCjWt4pUBDu5cTtSxAq_L93PPG_prg7VEI' }}
+				defaultCenter={center}
+				defaultZoom={zoom}
+			>
+				<Marker
+					lat={37.546066}
+					lng={127.0867}
+					name="The Graphic Novels"
+					color="blue"
+				/>
+			</GoogleMapReact>
+		</div>
+	);
 };
 
 export default MapComponent;
