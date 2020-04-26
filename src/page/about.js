3c6540d539bedca103c1f10a081b570eea946156
Seo hyunh ho <hyunh84@hanmail.net>
2020-04-26 12:15:21 +0900

    About 배경 및 sns아이콘 추가
    태그 및 검색 영역 타이틀 및 아카이브리스트와 소스 싱크 맞춤

--- src/page/about.js
+++ src/page/about.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const About = ({ action }) => {
@@ -12,15 +12,25 @@ const About = ({ action }) => {
 					</span>
 					<div className="intro">
 						<div className="scrollBox">
-							We describe ourselves as creative consultants. Not designers, or
-							writers, or advertisers, or brand strategists but all of these and
-							more. While our design roots were established with print associated
-							work, the growth of our business has branched out to include
-							branding, packaging and signage. More recently, we have made the
-							transition to include web design as an extension of our design
-							services, however design for print remains our first love and is the
-							core of our business – manifested mostly into design for branding,
-							packaging and associated collateral.
+							<div className="introDesc">
+								We describe ourselves as creative consultants. Not designers, or
+								writers, or advertisers, or brand strategists but all of these and
+								more. While our design roots were established with print associated
+								work, the growth of our business has branched out to include
+								branding, packaging and signage. More recently, we have made the
+								transition to include web design as an extension of our design
+								services, however design for print remains our first love and is the
+								core of our business – manifested mostly into design for branding,
+								packaging and associated collateral.
+							</div>
+							<div className="snsBox">
+								<Link to="/" className="btnInsta">
+									<img src="/resources/images/icon_insta.svg" alt="instagram" />
+								</Link>
+								<Link to="/" className="btnBe">
+									<img src="/resources/images/icon_Be.svg" alt="instagram" />
+								</Link>
+							</div>
 						</div>
 					</div>
 					{/* <div className="snsBox">
--- src/page/archive_search.js
+++ src/page/archive_search.js
@@ -1,114 +1,137 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useRef, useState, useContext } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
+import { masonryFn } from 'common';
 import { archiveListQuery } from 'apollo/archiveQuery';
 import TagMenu from 'components/tag_menu';
 import NaviList from 'components/naviList';
 import { Store } from 'store';
 
 const ArchiveSearch = ({ history, location: { search } }) => {
-  const [imgLoadComplate, setLoadComplate] = useState(0);
-  const [tag, setTag] = useState('');
-  const keyword = new URLSearchParams(search).get('keyword');
+	const archiveList = useRef(null);
+	const [imgLoadComplate, setLoadComplate] = useState(0);
+	const [tag, setTag] = useState('');
+	const keyword = new URLSearchParams(search).get('keyword');
 
-  useEffect(() => {
-    setTag('');
-  }, [keyword]);
+	useEffect(() => {
+		setTag('');
+	}, [keyword]);
 
-  const { data, loading, refetch } = useQuery(archiveListQuery, {
-    variables: { keyword },
-    fetchPolicy: 'network-only',
-  });
+	const { data, loading, refetch } = useQuery(archiveListQuery, {
+		variables: { keyword },
+		fetchPolicy: 'network-only',
+	});
 
-  const { setAction } = useContext(Store);
+	const { setAction } = useContext(Store);
 
-  useEffect(() => {
-    window.onpopstate = e => {
-      setAction(1);
-      history.push('/');
-    };
-  }, [history, setAction]);
+	useEffect(() => {
+		setLoadComplate(0);
+		window.onpopstate = e => {
+			setAction(1);
+			history.push('/');
+		};
+	}, [history, setAction]);
 
-  if (!loading) {
-    return (
-      <div className="contents searchResultWrap">
-        <span id="jumpConts" className="blind">
-          상세 본문영역
-        </span>
+	useEffect(() => {
+		let archiveListFn;
+		if (imgLoadComplate) {
+			// console.log('masonryFn');
+			archiveListFn = masonryFn(archiveList);
+		}
 
-        <NaviList />
+		return () => {
+			// console.log('useEffect 02 return');
+			if (archiveListFn) {
+				archiveListFn.destroy();
+			}
+		};
+	}, [imgLoadComplate]);
 
-        <Link to="/" onClick={() => setAction(1)} className="subMenu01">
-          <em>Archive</em>
-        </Link>
+	if (!loading) {
+		return (
+			<div className="contents searchResultWrap">
+				<span id="jumpConts" className="blind">
+					상세 본문영역
+				</span>
 
-        <div className="registBox">
-          <h2>
-            {data && data.seePortpoliosList.length} SEARCH RESULTS FOR:
-            {keyword && tag !== '' ? ` ${keyword} & ${tag}` : ` ${keyword}`}
-            <br />
-          </h2>
-        </div>
+				<NaviList />
 
-        <div
-          className="pageWrap active"
-          style={{ display: 'block', width: '100%', height: 'auto' }}
-        >
-          <div className="archiveWrap">
-            <TagMenu refetch={refetch} setTag={setTag} />
+				<Link to="/" onClick={() => setAction(1)} className="subMenu01">
+					<em>Archive</em>
+				</Link>
 
-            <div className="archiveListWrap">
-              <ul className="grid">
-                {data &&
-                  data.seePortpoliosList.map(portpolioData => (
-                    <li key={portpolioData._id} className="grid-item">
-                      <Link to={`/archiveDetail/${portpolioData._id}`}>
-                        <img
-                          src={portpolioData.thumbImg}
-                          onLoad={() => setLoadComplate(imgLoadComplate + 1)}
-                          alt={portpolioData.title}
-                        />
+				<div className="registBox">
+					<h2>
+						{data && data.seePortpoliosList.length} SEARCH RESULTS FOR:
+						{keyword && tag !== '' ? ` ${keyword} & ${tag}` : ` ${keyword}`}
+						<br />
+					</h2>
+				</div>
 
-                        <div className="itemFrame">
-                          <div className="inner">
-                            <span className="tits">{portpolioData.title}</span>
-                            <span className="tags">
-                              {portpolioData.tags.length > 0 &&
-                                portpolioData.tags.map((item, i) => {
-                                  if (i === 0) {
-                                    return item.value;
-                                  }
-                                  return `, ${item.value}`;
-                                })}
-                            </span>
-                          </div>
-                        </div>
-                      </Link>
-                    </li>
-                  ))}
-              </ul>
-            </div>
-            {/* {data && data.seePortpolios.totalPages > 1 && (
-              <Paging
-                nowPageNum={nowPageNum}
-                totalPage={data.seePortpolios.totalPages}
-                setPageNum={setPageNum}
-                setLoadComplate={setLoadComplate}
-              />
-            )} */}
-          </div>
-        </div>
+				<div
+					className="pageWrap active"
+					style={{ display: 'block', width: '100%', height: 'auto' }}
+				>
+					<div className="archiveWrap">
+						<TagMenu refetch={refetch} setTag={setTag} />
 
-        <Link to="/" onClick={() => setAction(2)} className="subMenu02">
-          <em>About</em>
-        </Link>
-        <Link to="/" onClick={() => setAction(3)} className="subMenu03">
-          <em>Contact</em>
-        </Link>
-      </div>
-    );
-  }
-  return '';
+						<div className="archiveListWrap">
+							<ul className="grid" ref={archiveList}>
+								{data &&
+									data.seePortpoliosList.map((portpolioData, i) => (
+										<li key={portpolioData._id} className="grid-item">
+											<Link to={`/archiveDetail/${portpolioData._id}`}>
+
+												<span className="img">
+													<img
+														id={`achiveImg${i}`}
+														src={portpolioData.thumbImg}
+														// onLoad={(e) => loadImagesFn(e, imgLoadComplate + 1)}
+														onLoad={() => setLoadComplate(imgLoadComplate + 1)}
+														alt={portpolioData.title}
+													/>
+												</span>
+
+												<div className="itemFrame">
+													<div className="inner">
+														<span className="tits">{portpolioData.title}</span>
+														<span className="tags">
+															{portpolioData.tags.length > 0 &&
+																portpolioData.tags.map((item, i) => {
+																	if (i === 0) {
+																		return item.value;
+																	}
+																	return `, ${item.value}`;
+																})}
+														</span>
+													</div>
+												</div>
+											</Link>
+										</li>
+									))}
+							</ul>
+						</div>
+						{/* {data && data.seePortpolios.totalPages > 1 && (
+							<Paging
+								nowPageNum={nowPageNum}
+								totalPage={data.seePortpolios.totalPages}
+								setPageNum={setPageNum}
+								setLoadComplate={setLoadComplate}
+							/>
+						)} */}
+					</div>
+				</div>
+
+				<Link to="/" onClick={() => setAction(2)} className="subMenu02">
+					<em>About</em>
+				</Link>
+				<Link to="/" onClick={() => setAction(3)} className="subMenu03">
+					<em>Contact</em>
+				</Link>
+			</div>
+		);
+	}
+	return '';
 };
 
 export default withRouter(ArchiveSearch);
